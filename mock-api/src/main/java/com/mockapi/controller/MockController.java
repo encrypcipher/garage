6d9178dfe5751614bf9978ddfa9ea7264e95deab
Vuyyur <vishnu.vuyyur@cgi.com>
2020-08-17 17:08:53 +0200

    enable cors origins between app and api

--- mock-api/src/main/java/com/mockapi/controller/MockController.java
+++ mock-api/src/main/java/com/mockapi/controller/MockController.java
@@ -1,7 +1,8 @@
 package com.mockapi.controller;
 
-import java.io.File;
-import java.nio.file.Files;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import org.json.simple.JSONArray;
 import org.json.simple.parser.JSONParser;
@@ -13,12 +14,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/mock")
 public class MockController {
-	
+
 	@GetMapping("/garages")
 	public JSONArray getCars() {
 		try {
-			File resource = new ClassPathResource("garages.json").getFile();
-			String warehouses = new String(Files.readAllBytes(resource.toPath()));
+//			File resource = new ClassPathResource("garages.json").getFile();
+//			String warehouses = new String(Files.readAllBytes(resource.toPath()));
+			InputStream resource = new ClassPathResource("garages.json").getInputStream();
+			String warehouses;
+			try (BufferedReader reader = new BufferedReader(new InputStreamReader(resource))) {
+				StringBuilder sb = new StringBuilder();
+				String line;
+				while ((line = reader.readLine()) != null) {
+					sb.append(line);
+					sb.append(System.lineSeparator());
+				}
+				warehouses = sb.toString().trim();
+			}
 			JSONParser parser = new JSONParser();
 			return (JSONArray) parser.parse(warehouses);
 		} catch (Exception ex) {
